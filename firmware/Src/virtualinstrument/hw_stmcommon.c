2bdfd4d086e574c48fe3a787cdbc8029ea707cde
Martin Cejp <cejpmart@fel.cvut.cz>
2017-05-05 17:06:36 +0200

    I want to die

--- firmware/Src/virtualinstrument/hw_stmcommon.c
+++ firmware/Src/virtualinstrument/hw_stmcommon.c
@@ -29,32 +29,62 @@ int HWGetPeriodPulseWidth(uint64_t* period_out, uint64_t* pulse_width_out) {
     uint64_t sum_period = 0;
     uint64_t sum_pulse_width = 0;
 
-    for (size_t i = 1; i < dma_num_samples + 1; i++) {
+    for (size_t i = 2; i < dma_num_samples + 2; i++) {
+#ifdef AUTO_RESET_IC_CNT
         // +2 is correct, but why and how?
-        //static const uint32_t mask = 0xffffffff;
-
-        uint32_t period = dmabuf[2 * i] + 2;//(dmabuf[2 * i] - dmabuf[2 * (i - 1)]) & mask;
-        uint32_t pulse_width = dmabuf[2 * i + 1] + 2;//(dmabuf[2 * i + 1] - dmabuf[2 * (i - 1)]) & mask;
+        uint32_t period = dmabuf[2 * i] + 2;
+        uint32_t pulse_width = dmabuf[2 * i + 1] + 2;
+#else
+        static const uint32_t mask = 0xffffffff;
+        uint32_t period = (dmabuf[2 * i] - dmabuf[2 * (i - 1)]) & mask;
+        uint32_t pulse_width = (dmabuf[2 * i + 1] - dmabuf[2 * (i - 1)]) & mask;
+#endif
 
         sum_period += period;
         sum_pulse_width += pulse_width;
     }
 
-    *period_out = (sum_period << 16) / dma_num_samples;
-    *pulse_width_out = (sum_pulse_width << 16) / dma_num_samples;
+    *period_out = (sum_period << 16) / measurement_num_samples;
+    *pulse_width_out = (sum_pulse_width << 16) / measurement_num_samples;
 
     return 1;
 }
 
-void HWInitPeriodMeasurement(size_t num_samples) {
+enum { HW_PRESCALER_MAX = 3 };
+
+static uint32_t GetHWPrescaler(size_t index) {
+    static const uint32_t hw_prescalers[] = {TIM_ICPSC_DIV1, TIM_ICPSC_DIV2, TIM_ICPSC_DIV4, TIM_ICPSC_DIV8};
+    return hw_prescalers[index];
+}
+
+int HWInitPeriodMeasurement(size_t num_samples) {
     TIM_ClockConfigTypeDef sClockSourceConfig;
     TIM_IC_InitTypeDef sConfigIC;
+
+    HAL_DMA_Abort(&INPUT_CAPTURE_HDMA);
+    __HAL_DMA_CLEAR_FLAG(&INPUT_CAPTURE_HDMA, INPUT_CAPTURE_DMA_TC_FLAG);
+
+    dma_num_samples = num_samples;
+    measurement_num_samples = num_samples;
+
+    int hw_prescaler = 0;
+
+#ifndef AUTO_RESET_IC_CNT
+    while (dma_num_samples % 2 == 0 && hw_prescaler < HW_PRESCALER_MAX) {
+        dma_num_samples /= 2;
+        hw_prescaler++;
+    }
+#endif
+
+    if ((1 + dma_num_samples) * 8 > sizeof(dmabuf))
+        return -1;
 
     // clock
     sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
     HAL_TIM_ConfigClockSource(&INPUT_CAPTURE_HTIM, &sClockSourceConfig);
 
-    // slave mode reset - this way, missed pulses don't matter
+#ifdef AUTO_RESET_IC_CNT
+    // slave mode reset - this way, missed pulses don't matter, but HW prescaling can't be used
     TIM_SlaveConfigTypeDef slaveConfig;
     slaveConfig.SlaveMode = TIM_SLAVEMODE_RESET;
     slaveConfig.InputTrigger = TIM_TS_TI2FP2;
@@ -62,11 +92,12 @@ void HWInitPeriodMeasurement(size_t num_samples) {
     slaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_RISING;
     slaveConfig.TriggerPrescaler = TIM_TRIGGERPRESCALER_DIV1;
     HAL_TIM_SlaveConfigSynchronization(&INPUT_CAPTURE_HTIM, &slaveConfig);
+#endif
 
     // input capture
     sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_FALLING;
     sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
-    sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+    sConfigIC.ICPrescaler = GetHWPrescaler(hw_prescaler);
     sConfigIC.ICFilter = 0;
     HAL_TIM_IC_ConfigChannel(&INPUT_CAPTURE_HTIM, &sConfigIC, INPUT_CAPTURE_FALLING_CHAN);
 
@@ -84,13 +115,8 @@ void HWInitPeriodMeasurement(size_t num_samples) {
     //INPUT_CAPTURE_HDMA.XferCpltCallback = XferCpltCallback;
     //__HAL_DMA_ENABLE_IT(&INPUT_CAPTURE_HDMA, DMA_IT_TC);
 
-    assert_param((1 + num_samples) * 8 <= sizeof(dmabuf));
-    dma_num_samples = num_samples;
-
-    HAL_DMA_Abort(&INPUT_CAPTURE_HDMA);
-    __HAL_DMA_CLEAR_FLAG(&INPUT_CAPTURE_HDMA, INPUT_CAPTURE_DMA_TC_FLAG);
-
-    HAL_DMA_Start(&INPUT_CAPTURE_HDMA, (uint32_t) &INPUT_CAPTURE_TIMER->DMAR, (uint32_t) dmabuf, (1 + num_samples) * 2);
+    HAL_DMA_Start(&INPUT_CAPTURE_HDMA, (uint32_t) &INPUT_CAPTURE_TIMER->DMAR, (uint32_t) dmabuf, (2 + dma_num_samples) * 2);
+    return 1;
 }
 
 void HWSetGeneratorPWM(uint16_t prescaler, uint16_t period, uint16_t pulse_time, int phase) {
